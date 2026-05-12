15e1e0829590f5c409166d020d3056f0eb80a2af
Tejas Mhaske <tejas.ycmku22@sinhgad.edu>
2026-05-12 15:52:16 +0530

    new changes

--- backend/models/Folder.js
+++ backend/models/Folder.js
@@ -1,7 +1,14 @@
+const mongoose = require("mongoose");
+
 const folderSchema = new mongoose.Schema({
-  name: String,
-  categoryId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category"
-  }
+
+    name: String,
+
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category"
+    }
+
 });
+
+module.exports = mongoose.model("Folder", folderSchema);
